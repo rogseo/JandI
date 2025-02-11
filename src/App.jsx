cc6609fd62af4e2b18a37fb71e9c3d80a455c415
rogseo <rogseo@gmail.com>
2025-02-10 20:11:00 -0600

    add service page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Service from "./pages/Service"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
         </Routes>
 
       </Router>
---
+++ src/pages/Service.jsx
@@ -0,0 +1,17 @@
+// src/pages/Service.jsx
+import pic from "../img/1.png"
+function Service() {
+    return (
+        <div className="absolute inset-x-0 z-0" >
+            <img src={pic} alt="Company Logo" className="w-full h-auto rounded-lg"/>
+            <div className="absolute top-0 left-0 w-full h-full text-end items-center justify-center  bg-opacity-50 rounded-lg">
+                <h2 className="text-white text-2xl font-bold text-balance">We Deliver Preconstruction Services</h2>
+                <h2 className="text-white text-2xl font-bold text-balance">for Realizing Customer Value</h2>
+            </div>
+    
+        </div>
+
+    );
+}
+
+export default Service;
